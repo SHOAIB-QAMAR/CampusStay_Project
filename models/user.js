74f030674bb8c61370b504e246f8615bc476ab10
Shoaib <shoaibqamar539@gmail.com>
2025-10-23 22:45:52 +0530

    Add 'landlord' role to user model

--- models/user.js
+++ models/user.js
@@ -10,7 +10,7 @@ const userSchema = new Schema({
     },
     role: {
         type: String,
-        enum: ['user', 'admin'],
+        enum: ['user', 'admin', 'landlord'],
         default: 'user'
     }
 })
